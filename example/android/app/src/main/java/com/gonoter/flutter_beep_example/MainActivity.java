9e02f1a8b97895940f27c1c9c982504272528f1e
b3arco <b3arco@users.noreply.github.com>
2021-03-23 23:01:42 +0100

    Re-initialize android/ in example app using 'flutter create'

--- example/android/app/src/main/java/com/gonoter/flutter_beep_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.gonoter.flutter_beep_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ example/android/app/src/main/kotlin/com/gonoter/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gonoter.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
