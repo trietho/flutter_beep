059b3f795bbc0a259aaf907a82de13918a9d3978
b3arco <b3arco@users.noreply.github.com>
2021-03-23 22:45:12 +0100

    Migrate example app to null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_beep/flutter_beep.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +12,27 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Beep Plugin example app'),
         ),
         body: Center(
-          child: Column(children: <Widget>[
-            RaisedButton( child: Text("Beep Success"), onPressed: ()=> FlutterBeep.beep()),
-            RaisedButton( child: Text("Beep Fail"), onPressed: ()=> FlutterBeep.beep(false)),
-            RaisedButton( child: Text("Beep Android Custom"), onPressed: ()=> FlutterBeep.playSysSound(AndroidSoundIDs.TONE_CDMA_ABBR_ALERT)),
-            RaisedButton( child: Text("Beep somthing"), onPressed: ()=> FlutterBeep.playSysSound(41)),
-            RaisedButton( child: Text("Beep iOS Custom"), onPressed: ()=> FlutterBeep.playSysSound(iOSSoundIDs.AudioToneBusy)),
-
-          ],),
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                  child: Text("Beep Success"),
+                  onPressed: () => FlutterBeep.beep()),
+              TextButton(
+                  child: Text("Beep Fail"),
+                  onPressed: () => FlutterBeep.beep(false)),
+              TextButton(
+                  child: Text("Beep Android Custom"),
+                  onPressed: () => FlutterBeep.playSysSound(
+                      AndroidSoundIDs.TONE_CDMA_ABBR_ALERT)),
+              TextButton(
+                  child: Text("Beep something"),
+                  onPressed: () => FlutterBeep.playSysSound(41)),
+              TextButton(
+                  child: Text("Beep iOS Custom"),
+                  onPressed: () =>
+                      FlutterBeep.playSysSound(iOSSoundIDs.AudioToneBusy)),
+            ],
+          ),
         ),
       ),
     );
--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,25 +1,39 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_beep_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_beep_example/main.dart';
-
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
+  testWidgets('Verify that app with FlutterBeep import renders',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+    await tester.pump();
 
     // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data.startsWith('Running on:'),
+        (Widget widget) =>
+            widget is Text &&
+            widget.data!.startsWith('Flutter Beep Plugin example app'),
+      ),
+      findsOneWidget,
+    );
+  });
+
+  testWidgets('Verify that call of FlutterBeep.beep() does not crash',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    await tester.pump();
+
+    await tester.tap(find.text("Beep Success"));
+    await tester.pump();
+
+    // Verify that platform version is retrieved.
+    expect(
+      find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is Text && widget.data!.startsWith('Beep Success'),
       ),
       findsOneWidget,
     );
