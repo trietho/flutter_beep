028042c1a4721551a5b507eab72379ca756f0b6d
Julian Steenbakker <jst@beverinnovations.com>
2022-02-01 09:09:54 +0100

    imp: upgrade android build version, dependencies and methodchannel registration

--- android/src/main/java/com/gonoter/flutter_beep/FlutterBeepPlugin.java
+++ android/src/main/java/com/gonoter/flutter_beep/FlutterBeepPlugin.java
@@ -9,11 +9,10 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterBeepPlugin */
 public class FlutterBeepPlugin implements FlutterPlugin, MethodCallHandler {
-  private ToneGenerator toneGen = new ToneGenerator(AudioManager.STREAM_SYSTEM, 100);
+  private final ToneGenerator toneGen = new ToneGenerator(AudioManager.STREAM_SYSTEM, 100);
 
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
@@ -23,22 +22,8 @@ public class FlutterBeepPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_beep");
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_beep");
     channel.setMethodCallHandler(this);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_beep");
-    channel.setMethodCallHandler(new FlutterBeepPlugin());
   }
 
   @Override
